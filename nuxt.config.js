36880ea711d220d84235c0042b53c027e2a8fa5d
saori-tisse <stera10@live.jp>
2020-05-08 02:25:06 +0900

    OGP設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,13 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { hid: 'og:site_name', property: 'og:site_name', content: 'parts stock' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: 'https://parts-stock.site/' },
+      { hid: 'og:title', property: 'og:title', content: 'parts stock' },
+      { hid: 'og:description', property: 'og:description', content: '共通ディスクリプション' },
+      { hid: 'og:image', property: 'og:image', content: 'https://parts-stock.site/img/ogp.png' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
